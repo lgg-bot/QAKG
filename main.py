526431187b009def1a7487b8a6736d9acca5d4ea
lvguoguang <lvguoguang@hust.edu.cn>
2020-04-08 09:55:46 +0800

    增加了外部机器人接口

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from qclassifier import *
 from qtransfer import *
 from qresult import *
+import json
+import requests
 
 import io
 import sys
@@ -15,12 +17,23 @@ class ChatBotGraph:
 
     def chat_main(self, sent):
         answer = '抱歉，暂时没有相关记载'
+        url_base = 'https://api.ownthink.com/bot?appid=40d71776962e06b5ba8a2bb5db206a2a&userid=user&spoken='
+        url = url_base + sent
+        sess = requests.get(url)
         res_classify = self.classifier.classify(sent)
         if not res_classify:
+            answer1 = sess.text
+            answer1 = json.loads(answer1)
+            answer2 = answer1['data']['info']['text']
+            answer = answer2.replace('小思','小艾')
             return answer
         res_sql = self.parser.parser_main(res_classify)
         final_answers = self.searcher.search_main(res_sql)
         if not final_answers:
+            answer1 = sess.text
+            answer1 = json.loads(answer1)
+            answer2 = answer1['data']['info']['text']
+            answer = answer2.replace('小思','小艾')
             return answer
         else:
             return '\n'.join(final_answers)
